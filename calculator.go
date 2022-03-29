41d5371691b1c66038422dfefd3f14e4b5101fac
jpschew <jps.chew@gmail.com>
2022-03-29 22:42:44 +0800

    added code to do arithmetic operation on two numbers

--- calculator.go
+++ calculator.go
@@ -1,25 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func add(a, b int) int {
 	//Insert code here
-
+	return a + b
 }
 
 func subtract(a, b int) int {
 	//Insert code here
-
+	return a - b
 }
 
 func multiply(a, b int) int {
 	//Insert code here
-
+	return a * b
 }
 
 func divide(a, b int) int {
 	//Insert code here
 	//consider for b = 0
+	if b == 0 {
+		fmt.Println("Zero Division Error")
+		return int(math.Inf(a))
+	}
+
+	return a / b
 
 }
 
@@ -34,4 +43,20 @@ func main() {
 	fmt.Scanln(&b)
 
 	//Insert code here
+	if process == "add" {
+		result := add(a, b)
+		fmt.Println(result)
+	} else if process == "sub" {
+		result := subtract(a, b)
+		fmt.Println(result)
+	} else if process == "mul" {
+		result := multiply(a, b)
+		fmt.Println(result)
+	} else if process == "div" {
+		result := divide(a, b)
+		fmt.Println(result)
+
+	} else {
+		fmt.Println("You did not enter the correct process! Please try again!")
+	}
 }
